65f1f7dc01a0d2454cb2621aec3720e7aec8c7fd
1dragon <dnjsdyd159@naver.com>
2024-05-20 16:23:12 +0900

    RestController -> Controller + ResponseBody

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -1,21 +1,22 @@
 package com.study.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
-@Controller
+//@Controller
+//@ResponseBody // 모든 메소드에 적용
+@RestController // Controller + ResponseBody
 @RequestMapping("/api/main45")
 public class Controller45 {
 
     @PostMapping("sub1")
-    @ResponseBody
+//    @ResponseBody
     public String sub1(@RequestParam("name") String name,
                        @RequestParam("file") MultipartFile file) throws IOException {
 
@@ -30,7 +31,7 @@ public class Controller45 {
     }
 
     @PostMapping("sub2")
-    @ResponseBody
+//    @ResponseBody
     public String sub2(@RequestParam("name") String name,
                        @RequestParam("file[]") MultipartFile[] file) throws IOException {
         System.out.println("name = " + name);
